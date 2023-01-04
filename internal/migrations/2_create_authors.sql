633100324385f6407ddf12189694833f380c6996
Kshitij Gang <kshitij@wednesday.is>
2023-01-04 17:12:39 +0530

    feat(database-migration): added roles,authors and posts

---
+++ internal/migrations/2_create_authors.sql
@@ -0,0 +1,21 @@
+-- +migrate Up
+-- SQL in section 'Up' is executed when this migration is applied
+CREATE TABLE  authors  (
+    id varchar(50) PRIMARY KEY,
+    username varchar(100) UNIQUE,
+    email varchar(100) UNIQUE,
+    name TEXT ,
+    active BOOLEAN,
+    author_address TEXT,
+    last_login TIMESTAMP,
+    last_password_change TIMESTAMP,
+    token TEXT,
+    role_id varchar(25),
+    created_at TIMESTAMP,
+	updated_at TIMESTAMP DEFAULT NOW(),
+	deleted_at TIMESTAMP, 
+    CONSTRAINT fk__role_users FOREIGN KEY (role_id) REFERENCES roles(id)
+);
+
+-- +migrate Down
+DROP TABLE authors;
--- internal/migrations/3_create_posts.sql
+++ internal/migrations/3_create_posts.sql
@@ -1,14 +1,15 @@
 -- +migrate Up
 -- SQL in section 'Up' is executed when this migration is applied
-CREATE TABLE roles (
+CREATE TABLE posts (
 			id varchar(50) PRIMARY KEY,
-			access_level int NOT NULL,
-			name text  NOT NULL,
+			title text NOT NULL,
+			content text  NOT NULL,
+            author_id varchar(50) NOT NULL,
 			created_at TIMESTAMP DEFAULT NOW(),
 			updated_at TIMESTAMP,
 			deleted_at TIMESTAMP
-		);
+);
 
 -- +migrate Down
 -- SQL section 'Down' is executed when this migration is rolled back
-DROP TABLE roles;
+DROP TABLE posts;

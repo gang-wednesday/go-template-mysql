bb31eeda58f35499349253ed5555b60bb7b05eb8
Kshitij Gang <kshitij@wednesday.is>
2023-01-04 17:16:03 +0530

    fix(migration): added a forgotten foreign key

--- internal/migrations/3_create_posts.sql
+++ internal/migrations/3_create_posts.sql
@@ -8,6 +8,7 @@ CREATE TABLE posts (
 			created_at TIMESTAMP DEFAULT NOW(),
 			updated_at TIMESTAMP,
 			deleted_at TIMESTAMP
+			CONSTRAINT fk__authors_postss FOREIGN KEY (author_id) REFERENCES authors(id)
 );
 
 -- +migrate Down

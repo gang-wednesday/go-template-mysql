29e186c57cca6b83cd7274b96afe52dc5523e2dc
Kshitij Gang <kshitij@wednesday.is>
2023-01-09 10:45:05 +0530

    feat(post-mutation-test): added test for post mutation resolver

--- resolver/post_mutations.resolver_test.go
+++ resolver/post_mutations.resolver_test.go
@@ -2,6 +2,7 @@ package resolver
 
 import (
 	"context"
+	"fmt"
 	"go-template/gqlmodels"
 	"go-template/testutls"
 	"regexp"
@@ -12,26 +13,49 @@ import (
 )
 
 func TestCreatePost(t *testing.T) {
+	cases := []struct {
+		name    string
+		wantErr bool
+	}{
+		{
+			name:    "successfull post insert",
+			wantErr: false,
+		}, {
+			name:    "failed post insert",
+			wantErr: true,
+		},
+	}
 	db, mock, err := sqlmock.New()
 	if err != nil {
 		t.Fatal(err)
 	}
 	boil.SetDB(db)
-	mock.ExpectExec(regexp.
-		QuoteMeta("INSERT INTO `posts` (`title`,`content`,`author_id`," +
-			"`created_at`,`updated_at`,`deleted_at`) VALUES (?,?,?,?,?,?)")).
-		WithArgs().
-		WillReturnResult(sqlmock.NewResult(1, 1))
 	resolver1 := Resolver{}
-	t.Run("successfull role insert", func(t *testing.T) {
-		mockPost := gqlmodels.PostCreateInput{
-			Title:   &testutls.MockPost().Title,
-			Content: &testutls.MockPost().Content,
+	for _, tt := range cases {
+		if tt.name == "successfull post insert" {
+			mock.ExpectExec(regexp.
+				QuoteMeta("INSERT INTO `posts` (`title`,`content`,`author_id`," +
+					"`created_at`,`updated_at`,`deleted_at`) VALUES (?,?,?,?,?,?)")).
+				WithArgs().
+				WillReturnResult(sqlmock.NewResult(1, 1))
+		} else {
+			mock.ExpectExec(regexp.QuoteMeta("INSERT INTO `posts`")).
+				WithArgs().
+				WillReturnError(fmt.Errorf("connection error :could not insert into database"))
 		}
-		_, err := resolver1.Mutation().CreatePost(context.Background(), mockPost)
-		if err != nil {
-			t.Fatal(err)
-		}
-	})
+		t.Run(tt.name, func(t *testing.T) {
+			mockPost := gqlmodels.PostCreateInput{
+				Title:   &testutls.MockPost().Title,
+				Content: &testutls.MockPost().Content,
+			}
+			_, err := resolver1.Mutation().CreatePost(context.Background(), mockPost)
+			if err != nil {
+				if !tt.wantErr {
+					t.Fatal(err)
+				}
+			}
+		})
+
+	}
 
 }
